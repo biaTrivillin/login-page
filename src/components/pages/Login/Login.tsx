a73ef7409c94b67cd3ddd23cb75afad36182885a
BiaTrivillin <biatrivillin.code@gmail.com>
2024-04-10 20:32:36 -0300

    created index and glassmorphism css files

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -1,3 +1,5 @@
+import "../../../styles/glassmorphism.css"
+
 function Login() {
 
 	return (
@@ -6,37 +8,34 @@ function Login() {
 				<div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
 					<div className="row gx-lg-5 align-items-center mb-5">
 						<div className="col-lg-6 mb-5 mb-lg-0">
-							<h1 className="my-5 display-5 fw-bold ls-tight">
-							The best offer <br />
-							<span>for your business</span>
+							<h1 className="my-5 display-5 fw-bold ls-tight text-light">
+							Nature's Finest,  <br />
+							<span>Yours to Flourish</span>
 							</h1>
-							<p className="mb-4 opacity-70">
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-							Temporibus, expedita iusto veniam atque, magni tempora mollitia
-							dolorum consequatur nulla, neque debitis eos reprehenderit quasi
-							ab ipsum nisi dolorem modi. Quos?
+							<p className="mb-5 fs-6 text-light fw-semibold">
+							Welcome to GreenHaven, your trusted source for a lush variety of plants that bring life and serenity to your space. Explore our diverse selection of species, carefully cultivated to thrive in any environment. Start your green journey with us today!
 							</p>
 						</div>
 
 						<div className="col-lg-6 mb-5 mb-lg-0 position-relative">
-							<div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
-							<div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
-							<div className="card bg-glass">
+							{/* <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
+							<div id="radius-shape-2" className="position-absolute shadow-5-strong"></div> */}
+							<div className="card bg-transparent border-0 card_container">
 								<div className="card-body px-4 py-5 px-md-5">
 									<form>
 										<div data-mdb-input-init className="form-outline mb-4">
 											<input type="email" id="form3Example3" className="form-control" />
-											<label className="form-label" htmlFor="form3Example3">Email address</label>
+											<label className="form-label text-light" htmlFor="form3Example3">Email address</label>
 										</div>
 
 										<div data-mdb-input-init className="form-outline mb-4">
 											<input type="password" id="form3Example4" className="form-control" />
-											<label className="form-label" htmlFor="form3Example4">Password</label>
+											<label className="form-label text-light" htmlFor="form3Example4">Password</label>
 										</div>
 
 										<div className="form-check d-flex justify-content-center mb-4">
 											<input className="form-check-input me-2" type="checkbox" value="" id="form2Example33"/>
-											<label className="form-check-label" htmlFor="form2Example33">
+											<label className="form-check-label text-light" htmlFor="form2Example33">
 											Lembre minha senha
 											</label>
 										</div>
@@ -46,7 +45,7 @@ function Login() {
 										</button>
 
 										<div className="text-center">
-											<p>or sign up with:</p>
+											<p className="text-light">or sign up with:</p>
 											<button type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-link btn-floating mx-1">
 											<i className="fab fa-facebook-f"></i>
 											</button>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import MainRoutes from "./Routes.tsx"; 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
