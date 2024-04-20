4ff2315c5ed4ab0dc7180d409e332123c71b9b52
BiaTrivillin <biatrivillin.code@gmail.com>
2024-04-20 10:23:50 -0300

    created register function in server index.js for mysql data base

--- server/index.js
+++ server/index.js
@@ -6,12 +6,58 @@ const db = mysql.createPool({
     host: "localhost",
     user: "admin",
     password: "meubanco123",
-    database: ""
+    database: "users"
 })
 
-server.get("/", (req, res) => {
-    res.send("hello")
+// server.get("/", (req, res) => {
+//     db.query(
+//         "INSERT INTO dados (firstname, lastname, email, password) VALUES ('Beatriz', 'Trivillin', 'bia@gmail.com', 'bia123')", (err, result) => {
+//             if (err) {
+//                 console.log(err)
+//             }
+//         }
+//     )
+// })
+
+server.use(express.json());
+server.use(cors());
+
+server.post("/register", (req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    db.query("INSERT INTO dados (firstname, lastname, email, password) VALUES (?, ?, ?, ?)", [firstName, lastName, email, password], (err, result) => {
+        if (err) {
+            res.send(err)
+        }
+        res.send({msg: "cadastro realizado"});
+    })
 })
+// server.post("/register", (req, res) => {
+//     const firstName = req.body.firstName;
+//     const lastName = req.body.lastName;
+//     const email = req.body.email;
+//     const password = req.body.password;
+
+//     db.query("SELECT * FROM dados WHERE email = ?", [email], (err, result) => {
+//         if(err) {
+//             res.send(err);
+//         }
+//         if(result.length == 0) {
+//             db.query("INSERT INTO dados (firstname, lastname, email, password) VALUES (?, ?, ?, ?)", [firstName, lastName, email, password], (err, result) => {
+//                 if (err) {
+//                     res.send(err)
+//                 }
+//                 res.send({msg: "cadastro realizado"});
+//             })
+//         }else {
+//             res.send({msg: "email já cadastrado"})
+//         }
+
+//     })
+// })
 
 server.listen(3000, () => {
     console.log("Server iniciado e rodando na porta 3000!")
